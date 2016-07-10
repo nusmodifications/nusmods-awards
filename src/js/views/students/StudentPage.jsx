1ff4855b66638504584c01fef4b8fe5fe0e3d875
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-07-10 23:18:57 +0800

    Show error if name does not match student

--- src/js/views/students/StudentPage.jsx
+++ src/js/views/students/StudentPage.jsx
@@ -10,63 +10,69 @@ export default class StudentPage extends Component {
 
     return (
       <div className="main-content">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <h2>{studentName}</h2>
-              <br/>
+        {studentRecords.length ?
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h2>{studentName}</h2>
+                <br/>
+              </div>
+            </div>
+            <div className="row">
+              {studentRecords.map((studentRecord) => {
+                return studentRecord.Awards.map((award, i) => {
+                  const facultyLink = (
+                    <Link className={studentRecord.Faculty.toLowerCase()}
+                      to={`/${studentRecord.Faculty.toLowerCase()}`}>
+                      {studentRecord.Faculty}
+                    </Link>
+                  );
+
+                  return (
+                    <div className="col-md-6" key={i}>
+                      <div className="card card-block">
+                        {award.Type === 'Dean\'s List Award' ?
+                          <div>
+                            <h4 className="card-title">{award.Type}</h4>
+                            <p className="card-text">{award.AcadYear} Sem {award.Sem} &middot; {facultyLink}</p>
+                          </div> : null
+                        }
+                        {award.Type === 'Faculty Award' ?
+                          <div>
+                            <h4 className="card-title">{award.AwardName}</h4>
+                            <p className="card-text">{award.AcadYear} &middot; {facultyLink}</p>
+                          </div> : null
+                        }
+                        {award.Type === 'Commencement Award' ?
+                          <div>
+                            <h4 className="card-title">{award.AwardName}</h4>
+                            <p className="card-text">{award.AcadYear} &middot; {facultyLink}</p>
+                            <p className="card-text">
+                              <small className="text-muted">{award.AwardDesc}</small>
+                            </p>
+                          </div> : null
+                        }
+
+                      </div>
+                    </div>
+                  );
+                });
+              })}
+            </div>
+            <hr/>
+            <div>
+              <button className="btn btn-primary" onClick={() => {
+                this.context.router.push({
+                  pathname: `/${studentRecords[0].Faculty.toLowerCase()}`
+                });
+              }}>Back</button>
             </div>
           </div>
-          <div className="row">
-            {studentRecords.map((studentRecord) => {
-              return studentRecord.Awards.map((award, i) => {
-                const facultyLink = (
-                  <Link className={studentRecord.Faculty.toLowerCase()}
-                    to={`/${studentRecord.Faculty.toLowerCase()}`}>
-                    {studentRecord.Faculty}
-                  </Link>
-                );
-
-                return (
-                  <div className="col-md-6" key={i}>
-                    <div className="card card-block">
-                      {award.Type === 'Dean\'s List Award' ?
-                        <div>
-                          <h4 className="card-title">{award.Type}</h4>
-                          <p className="card-text">{award.AcadYear} Sem {award.Sem} &middot; {facultyLink}</p>
-                        </div> : null
-                      }
-                      {award.Type === 'Faculty Award' ?
-                        <div>
-                          <h4 className="card-title">{award.AwardName}</h4>
-                          <p className="card-text">{award.AcadYear} &middot; {facultyLink}</p>
-                        </div> : null
-                      }
-                      {award.Type === 'Commencement Award' ?
-                        <div>
-                          <h4 className="card-title">{award.AwardName}</h4>
-                          <p className="card-text">{award.AcadYear} &middot; {facultyLink}</p>
-                          <p className="card-text">
-                            <small className="text-muted">{award.AwardDesc}</small>
-                          </p>
-                        </div> : null
-                      }
-
-                    </div>
-                  </div>
-                );
-              });
-            })}
+          :
+          <div className="text-sm-center">
+            <h3>Student not found :(</h3>
           </div>
-          <hr/>
-          <div>
-            <button className="btn btn-primary" onClick={() => {
-              this.context.router.push({
-                pathname: `/${studentRecords[0].Faculty.toLowerCase()}`
-              });
-            }}>Back</button>
-          </div>
-        </div>
+        }
       </div>
     )
   }
